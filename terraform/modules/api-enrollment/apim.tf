bd3989c9fa1538c58e26554edc4bd423baca8c1c
Luke Samuels <lukesamuels@macbookpro.mynetworksettings.com>
2025-10-20 14:06:25 -0400

    K12-3580 Add self-signed cert to APIM for local development

--- terraform/modules/api-enrollment/apim.tf
+++ terraform/modules/api-enrollment/apim.tf
@@ -14,6 +14,66 @@ resource "azurerm_api_management" "api_enrollment" {
   }
 }
 
+#K12-3580 - Generate self-signed certficate for local environments to talk to APIM
+resource "azurerm_key_vault_certificate" "apim_cert" {
+  name         = "${var.environment_name}-apim-cert"
+  key_vault_id = azurerm_key_vault.api_enrollment_kv.id
+
+  certificate_policy {
+    issuer_parameters {
+      name = "Self"
+    }
+
+    key_properties {
+      exportable = true
+      key_size   = 2048
+      key_type   = "RSA"
+      reuse_key  = true
+    }
+
+    lifetime_action {
+      action {
+        action_type = "AutoRenew"
+      }
+
+      trigger {
+        days_before_expiry = 30
+      }
+    }
+
+    secret_properties {
+      content_type = "application/x-pkcs12"
+    }
+
+    x509_certificate_properties {
+      # Server Authentication = 1.3.6.1.5.5.7.3.1
+      # Client Authentication = 1.3.6.1.5.5.7.3.2
+      extended_key_usage = ["1.3.6.1.5.5.7.3.1"]
+
+      key_usage = [
+        "cRLSign",
+        "dataEncipherment",
+        "digitalSignature",
+        "keyAgreement",
+        "keyCertSign",
+        "keyEncipherment",
+      ]
+
+      subject            = "CN=${var.environment_name}-api-enrollment.azurewebsites.net"
+      validity_in_months = 12
+    }
+  }
+}
+
+# Assign certificate to APIM
+resource "azurerm_api_management_certificate" "apim_cert" {
+  name                = "${var.environment_name}-apim-cert"
+  api_management_name = azurerm_api_management.api_enrollment.name
+  resource_group_name = var.resource_group_name
+
+  key_vault_secret_id = azurerm_key_vault_certificate.apim_cert.secret_id
+}
+
 resource "azurerm_api_management_api_version_set" "api-enrollment" {
   name                = "${var.environment_name}-api-enrollment"
   resource_group_name = var.resource_group_name
